eb6e4356864d9b443e1977d3e9b21667ec1d76d4
wanghongxu <wang19930902@gmail.com>
2019-10-19 21:43:34 +0800

    add partition op

--- t2stor/tools/disk.py
+++ t2stor/tools/disk.py
@@ -3,18 +3,26 @@
 import logging
 import os
 
+from t2stor.tools.base import ToolBase
+from t2stor.exception import ProgrammingError
+
 
 logger = logging.getLogger(__name__)
 
 
-class DiskTool(object):
-    host = None
+class DiskTool(ToolBase):
+    host_prefix = None
 
-    def __init__(self, host=None):
-        self.host = host
+    def __init__(self, *args, host_prefix=None, **kwargs):
+        self.host_prefix = host_prefix
+        super(DiskTool, self).__init__(*args, **kwargs)
 
     def _wapper(self, path):
-        return os.path.join(self.host, path)
+        if not self.host_prefix:
+            return path
+        if path[0] == os.path.sep:
+            path = path[1:]
+        return os.path.join(self.host_prefix, path)
 
     def all(self):
         res = {}
@@ -42,11 +50,43 @@ class DiskTool(object):
                 }
         return res
 
-    def partition_create(self, disk):
-        pass
+    def partitions_create(self, disk, partitions):
+        """Create partitions
 
-    def partition_clear(self, disk):
-        pass
+        :param disk: disk name, eg sda.
+        :param partitions: partitions to create, eg ["0%", "20%", "100%"] will
+                           create two partitions, 0%-20% and 20%-100%.
+
+        """
+        for i in partitions:
+            if i[-1] != "%":
+                raise ProgrammingError(
+                    reason="partitions argument not end with %")
+        disk = self._wapper("/dev/%s" % disk)
+        cmd = "parted {} mklabel gpt ".format(disk)
+        start = partitions.pop(0)
+        while True:
+            end = partitions.pop(0)
+            cmd += " mkpart primary {} {}".format(start, end)
+            if len(partitions) == 0:
+                break
+            start = end
+        code, out, err = self.run_command(cmd)
+        if code:
+            logger.exception("Create partations error, Stdout: %s", out)
+            raise ProgrammingError(
+                reason="partitions argument not end with %")
+        return True
+
+    def partitions_clear(self, disk):
+        disk_path = self._wapper("/dev/%s" % disk)
+        cmd = "wipefs {} -a".format(disk_path)
+        code, out, err = self.run_command(cmd)
+        if code:
+            logger.exception("Create partations error, Stdout: %s", out)
+            raise ProgrammingError(
+                reason="partitions argument not end with %")
+        return False
 
 
 if __name__ == '__main__':
