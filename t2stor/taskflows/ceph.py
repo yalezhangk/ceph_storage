71377539a3e078dd960f980cac7c57578de417cd
wanghongxu <wang19930902@gmail.com>
2019-10-25 16:32:35 +0800

    add ceph config parser

--- t2stor/taskflows/ceph.py
+++ t2stor/taskflows/ceph.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import configparser
+from io import StringIO
+
+from t2stor import objects
 
 
 class CephTask(object):
+    ctxt = None
+
+    def __init__(self, ctxt):
+        self.ctxt = ctxt
+
+    def ceph_config(self):
+        configer = configparser.ConfigParser()
+        configs = objects.CephConfigList.get_all(self.ctxt)
+        for config in configs:
+            if not configer.has_section(config.group):
+                configer[config.group] = {}
+            configer[config.group][config.key] = config.value
+        buf = StringIO()
+        configer.write(buf)
+        return buf.getvalue()
 
     def pool_create(self):
         pass
