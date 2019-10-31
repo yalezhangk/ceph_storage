739aeaee875629d2a41a4a8babaef81d8d87786b
gongsichao <gongsichao@sinorail.com>
2019-10-31 15:32:37 +0800

    Add config type check

--- t2stor/utils/cluster_config.py
+++ t2stor/utils/cluster_config.py
@@ -2,13 +2,6 @@
 # cluster configs
 cluster_sys_configs = ['chrony_server', 'cluster_name', 'mon_available']
 
-cluster_configs = ['debug_ms', 'debug_osd', 'debug_bluestore', 'debug_mon',
-                   'debug_rbd', 'debug_rados', 'debug_perfcounter',
-                   't2ce_iobypass_size_kb', 't2ce_flush_water_level',
-                   't2ce_iobypass_water_level', 't2ce_gc_moving_skip',
-                   'bluestore_cache_size', 'bluestore_cache_size_ssd',
-                   'bluestore_cache_size_hdd']
-
 cluster_osd_restart_configs = ['bluestore_cache_size',
                                'bluestore_cache_size_ssd',
                                'bluestore_cache_size_hdd']
@@ -25,3 +18,9 @@ cluster_osd_temp_configs = ['debug_ms', 'debug_osd', 'debug_bluestore',
                             't2ce_iobypass_water_level', 't2ce_gc_moving_skip']
 
 cluster_mon_temp_configs = ['debug_ms', 'debug_mon', 'debug_perfcounter']
+
+cluster_configs = {
+    'debug_osd': {'type': int, 'default': 5},
+    'debug_mon': {'type': int, 'default': 5},
+    'osd_pool_default_type': {'type': str, 'default': 'replicated'},
+}
