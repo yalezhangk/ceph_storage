5a31de43b3a8384369a244cf6ba85e1b185d7119
wanghongxu <wang19930902@gmail.com>
2019-11-02 15:06:36 +0800

    re(admin): move action_log out of admin

--- t2stor/admin/admin.py
+++ t2stor/admin/admin.py
@@ -10,6 +10,7 @@ from oslo_log import log as logging
 
 from t2stor import exception
 from t2stor import objects
+from t2stor.admin.action_log import ActionLogHandler
 from t2stor.admin.alert_log import AlertLogHandler
 from t2stor.admin.datacenter import DatacenterHandler
 from t2stor.admin.disk import DiskHandler
@@ -42,7 +43,8 @@ class AdminQueue(queue.Queue):
     pass
 
 
-class AdminHandler(AlertLogHandler,
+class AdminHandler(ActionLogHandler,
+                   AlertLogHandler,
                    DatacenterHandler,
                    DiskHandler,
                    EmailHandler,
@@ -913,33 +915,6 @@ class AdminHandler(AlertLogHandler,
             'total_cluster_byte': total_cluster_byte,
             'pool_list': pool_list
         }
-
-    ###################
-
-    def action_log_get_all(self, ctxt, marker=None, limit=None,
-                           sort_keys=None, sort_dirs=None, filters=None,
-                           offset=None):
-        filters = filters or {}
-        filters['cluster_id'] = ctxt.cluster_id
-        return objects.ActionLogList.get_all(
-            ctxt, marker=marker, limit=limit, sort_keys=sort_keys,
-            sort_dirs=sort_dirs, filters=filters, offset=offset)
-
-    def action_log_create(self, ctxt, data):
-        data.update({'cluster_id': ctxt.cluster_id})
-        action_log = objects.ActionLog(ctxt, **data)
-        action_log.create()
-        return action_log
-
-    def action_log_get(self, ctxt, action_log_id):
-        return objects.ActionLog.get_by_id(ctxt, action_log_id)
-
-    def action_log_update(self, ctxt, action_log_id, data):
-        action_log = self.action_log_get(ctxt, action_log_id)
-        for k, v in six.iteritems(data):
-            setattr(action_log, k, v)
-        action_log.save()
-        return action_log
 
     ###################
 
