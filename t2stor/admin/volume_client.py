99145d7c753cab32891e529c898f66a484bff8ad
wanghongxu <wang19930902@gmail.com>
2019-11-02 16:39:04 +0800

    re(admin): move volume client out of admin

---
+++ t2stor/admin/volume_client.py
@@ -0,0 +1,53 @@
+from oslo_log import log as logging
+
+from t2stor import objects
+from t2stor.admin.base import AdminBaseHandler
+
+logger = logging.getLogger(__name__)
+
+
+class VolumeClientHandler(AdminBaseHandler):
+    def volume_client_group_get_all(self, ctxt, marker=None, limit=None,
+                                    sort_keys=None, sort_dirs=None,
+                                    filters=None, offset=None):
+        filters = filters or {}
+        return objects.VolumeClientGroupList.get_all(
+            ctxt, marker=marker, limit=limit, sort_keys=sort_keys,
+            sort_dirs=sort_dirs, filters=filters, offset=offset)
+
+    def volume_client_group_create(self, ctxt, data):
+        data.update({
+            'cluster_id': ctxt.cluster_id,
+        })
+        v = objects.VolumeClientGroup(ctxt, **data)
+        v.create()
+        return v
+
+    def volume_client_create(self, ctxt, data):
+        data.update({
+            'cluster_id': ctxt.cluster_id,
+        })
+        v = objects.VolumeClient(ctxt, **data)
+        v.create()
+        return v
+
+    def volume_client_group_get(self, ctxt, group_id):
+        return objects.VolumeClientGroup.get_by_id(ctxt, group_id)
+
+    def volume_client_group_delete(self, ctxt, group_id):
+        filters = {"volume_client_group_id": group_id}
+        # delete volume clients of the volume client group
+        for vc in objects.VolumeClientList.get_all(ctxt, filters=filters):
+            vc.destroy()
+        volume_client_group = objects.VolumeClientGroup.get_by_id(
+            ctxt, group_id)
+        volume_client_group.destroy()
+        return volume_client_group
+
+    def volume_client_get_all(self, ctxt, marker=None, limit=None,
+                              sort_keys=None, sort_dirs=None,
+                              filters=None, offset=None):
+        filters = filters or {}
+        return objects.VolumeClientList.get_all(
+            ctxt, marker=marker, limit=limit, sort_keys=sort_keys,
+            sort_dirs=sort_dirs, filters=filters, offset=offset)
