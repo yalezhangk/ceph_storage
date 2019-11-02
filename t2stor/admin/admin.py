e4cf7d458313b1c21399d247ded24e11417cf88c
wanghongxu <wang19930902@gmail.com>
2019-11-02 17:47:10 +0800

    re(admin): move prometheus out of admin

--- t2stor/admin/admin.py
+++ t2stor/admin/admin.py
@@ -19,6 +19,7 @@ from t2stor.admin.email_group import EmailGroupHandler
 from t2stor.admin.genconf import ceph_conf
 from t2stor.admin.osd import OsdHandler
 from t2stor.admin.pool import PoolHandler
+from t2stor.admin.prometheus import PrometheusHandler
 from t2stor.admin.rack import RackHandler
 from t2stor.admin.volume import VolumeHandler
 from t2stor.admin.volume_client import VolumeClientHandler
@@ -30,7 +31,6 @@ from t2stor.taskflows.ceph import CephTask
 from t2stor.taskflows.node import NodeTask
 from t2stor.tools.base import Executor
 from t2stor.tools.ceph import CephTool
-from t2stor.tools.prometheus import PrometheusTool
 from t2stor.utils.mail import send_mail
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
@@ -52,6 +52,7 @@ class AdminHandler(ActionLogHandler,
                    EmailGroupHandler,
                    OsdHandler,
                    PoolHandler,
+                   PrometheusHandler,
                    RackHandler,
                    VolumeHandler,
                    VolumeClientHandler,
@@ -475,97 +476,6 @@ class AdminHandler(ActionLogHandler,
             'total_cluster_byte': total_cluster_byte,
             'pool_list': pool_list
         }
-
-    ###################
-
-    def cluster_metrics_get(self, ctxt):
-        prometheus = PrometheusTool(ctxt)
-        res = {}
-        cluster_perf = {'cluster_read_bytes_sec', 'cluster_read_op_per_sec',
-                        'cluster_recovering_bytes_per_sec',
-                        'cluster_recovering_objects_per_sec',
-                        'cluster_write_bytes_sec', 'cluster_write_op_per_sec',
-                        'cluster_write_lat', 'cluster_read_lat',
-                        'cluster_total_bytes', 'cluster_total_used_bytes'}
-
-        for m in cluster_perf:
-            metric = 'ceph_{}'.format(m)
-            res[m] = prometheus.prometheus_get_metric(metric)
-        prometheus.cluster_get_pg_state(res)
-        return res
-
-    def cluster_history_metrics_get(self, ctxt, start, end):
-        prometheus = PrometheusTool(ctxt)
-        res = {}
-        cluster_perf = {'cluster_read_bytes_sec', 'cluster_read_op_per_sec',
-                        'cluster_recovering_bytes_per_sec',
-                        'cluster_recovering_objects_per_sec',
-                        'cluster_write_bytes_sec', 'cluster_write_op_per_sec',
-                        'cluster_write_lat', 'cluster_read_lat',
-                        'cluster_total_bytes', 'cluster_total_used_bytes'}
-        for m in cluster_perf:
-            metric = 'ceph_{}'.format(m)
-            res[m] = prometheus.prometheus_get_histroy_metric(
-                metric, float(start), float(end))
-        return res
-
-    def node_metrics_monitor_get(self, ctxt, node_id):
-        node = objects.Node.get_by_id(ctxt, node_id)
-        prometheus = PrometheusTool(ctxt)
-        metrics = prometheus.node_get_metrics_monitor(node)
-        return metrics
-
-    def node_metrics_histroy_monitor_get(self, ctxt, node_id, start, end):
-        node = objects.Node.get_by_id(ctxt, node_id)
-        prometheus = PrometheusTool(ctxt)
-        metrics = prometheus.node_get_metrics_histroy_monitor(
-            node, float(start), float(end))
-        return metrics
-
-    def node_metrics_network_get(self, ctxt, node_id, net_name):
-        node = objects.Node.get_by_id(ctxt, node_id)
-        prometheus = PrometheusTool(ctxt)
-        metrics = prometheus.node_get_metrics_network(node, net_name)
-        return metrics
-
-    def node_metrics_histroy_network_get(self, ctxt, node_id, net_name, start,
-                                         end):
-        node = objects.Node.get_by_id(ctxt, node_id)
-        prometheus = PrometheusTool(ctxt)
-        data = prometheus.node_get_metrics_histroy_network(
-            node=node, net_name=net_name, start=float(start), end=float(end))
-        return data
-
-    def osd_metrics_get(self, ctxt, osd_id):
-        osd = objects.Osd.get_by_id(ctxt, osd_id)
-        prometheus = PrometheusTool(ctxt)
-        data = prometheus.osd_get_realtime_metrics(osd)
-        return data
-
-    def osd_metrics_history_get(self, ctxt, osd_id, start, end):
-        osd = objects.Osd.get_by_id(ctxt, osd_id)
-        prometheus = PrometheusTool(ctxt)
-        data = prometheus.osd_get_histroy_metrics(osd, float(start),
-                                                  float(end))
-        return data
-
-    def pool_metrics_get(self, ctxt, pool_id):
-        pool = objects.Pool.get_by_id(ctxt, pool_id)
-        prometheus = PrometheusTool(ctxt)
-        metrics = {}
-        prometheus.pool_get_capacity(pool, metrics)
-        prometheus.pool_get_perf(pool, metrics)
-        return metrics
-
-    def pool_metrics_history_get(self, ctxt, pool_id, start, end):
-        pool = objects.Pool.get_by_id(ctxt, pool_id)
-        prometheus = PrometheusTool(ctxt)
-        metrics = {}
-        prometheus.pool_get_histroy_capacity(pool, float(start), float(end),
-                                             metrics)
-        prometheus.pool_get_histroy_perf(pool, float(start), float(end),
-                                         metrics)
-        return metrics
 
 
 class AdminService(ServiceBase):
