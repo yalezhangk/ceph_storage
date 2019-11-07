5601b4d1b3714243b9234a9efb0a5b9a9fa82958
wanghongxu <wanghongxu@t2cloud.net>
2019-11-08 00:20:20 +0800

    fix(build): build package

--- DSpace/version.py
+++ DSpace/version.py
@@ -5,5 +5,5 @@ T2STOR_PRODUCT = "T2STOR Portal"
 T2STOR_PACKAGE = None  # OS distro package version suffix
 
 loaded = False
-version_info = pbr_version.VersionInfo('DSpace')
+version_info = pbr_version.VersionInfo('Athena')
 version_string = version_info.version_string
