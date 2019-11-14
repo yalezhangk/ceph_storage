99a9cda63d3b3efb74397183f9fbfb838577cfe3
Wang Qiwei <wangqiwei@t2cloud.net>
2019-11-14 09:38:19 +0800

    add alert group api doc

--- DSpace/DSI/handlers/alert_group.py
+++ DSpace/DSI/handlers/alert_group.py
@@ -15,6 +15,40 @@ logger = logging.getLogger(__name__)
 class AlertGroupListHandler(ClusterAPIHandler):
     @gen.coroutine
     def get(self):
+        """
+        ---
+        tags:
+        - alert_group
+        summary: alert_group List
+        description: Return a list of alert_groups
+        operationId: alertgroups.api.listAlertGroup
+        produces:
+        - application/json
+        parameters:
+        - in: header
+          name: X-Cluster-Id
+          description: Cluster ID
+          schema:
+            type: string
+          required: true
+        - in: request
+          name: limit
+          description: Limit objects of response
+          schema:
+            type: integer
+            format: int32
+          required: false
+        - in: request
+          name: offset
+          description: Skip objects of response
+          schema:
+            type: integer
+            format: int32
+          required: false
+        responses:
+        "200":
+          description: successful operation
+        """
         ctxt = self.get_context()
         client = self.get_admin_client(ctxt)
         page_args = self.get_paginated_args()
@@ -29,6 +63,52 @@ class AlertGroupListHandler(ClusterAPIHandler):
 
     @gen.coroutine
     def post(self):
+        """
+        ---
+        tags:
+        - alert_group
+        summary: Create alert_group
+        description: Create alert_group.
+        operationId: alertgroups.api.createAlertGroup
+        produces:
+        - application/json
+        parameters:
+        - in: header
+          name: X-Cluster-Id
+          description: Cluster ID
+          schema:
+            type: string
+          required: true
+        - in: body
+          name: alert_group
+          description: Created alert_group object
+          required: true
+          schema:
+            type: object
+            properties:
+              alert_group:
+                type: object
+                description: alert_group object
+                properties:
+                  name:
+                    type: string
+                    description: alert_group's name
+                  alert_rule_ids:
+                    type: array
+                    items:
+                      type: integer
+                      format: int32
+                      description: alert_rule's ID
+                  email_group_ids:
+                    type: array
+                    items:
+                      type: integer
+                      format: int32
+                      description: email_group's ID
+        responses:
+        "200":
+          description: successful operation
+        """
         ctxt = self.get_context()
         data = json_decode(self.request.body)
         data = data.get("alert_group")
@@ -42,6 +122,33 @@ class AlertGroupListHandler(ClusterAPIHandler):
 class AlertGroupHandler(ClusterAPIHandler):
     @gen.coroutine
     def get(self, alert_group_id):
+        """
+        ---
+        tags:
+        - alert_group
+        summary: Detail of the alert_group
+        description: Return detail infomation of alert_group by id
+        operationId: alertgroups.api.alertGroupDetail
+        produces:
+        - application/json
+        parameters:
+        - in: header
+          name: X-Cluster-Id
+          description: Cluster ID
+          schema:
+            type: string
+          required: true
+        - in: url
+          name: id
+          description: alert_group ID
+          schema:
+            type: integer
+            format: int32
+          required: true
+        responses:
+        "200":
+          description: successful operation
+        """
         ctxt = self.get_context()
         client = self.get_admin_client(ctxt)
         expected_attrs = ['alert_rules', 'email_groups']
@@ -53,6 +160,74 @@ class AlertGroupHandler(ClusterAPIHandler):
 
     @gen.coroutine
     def put(self, email_group_id):
+        """
+        ---
+        tags:
+        - alert_group
+        summary: Update alert_group
+        description: Update alert_group.
+        operationId: alertgroups.api.updateAlertGroup
+        produces:
+        - application/json
+        parameters:
+        - in: header
+          name: X-Cluster-Id
+          description: Cluster ID
+          schema:
+            type: string
+          required: true
+        - in: body
+          name: alert_group(update name)
+          description: updated alert_group object. One Choice from Three.
+          required: true
+          schema:
+            type: object
+            properties:
+              alert_group:
+                type: object
+                description: alert_group object
+                properties:
+                  name:
+                    type: string
+                    description: update alert_group's name
+        - in: body
+          name: alert_group(update alert_rule)
+          description: updated alert_group object. One Choice from Three.
+          required: true
+          schema:
+            type: object
+            properties:
+              alert_group:
+                type: object
+                description: alert_group object
+                properties:
+                  alert_rule_ids:
+                    type: array
+                    items:
+                      type: integer
+                      format: int32
+                      description: update alert_group's alert_rule
+        - in: body
+          name: alert_group(update email_group)
+          description: updated alert_group object. One Choice from Three.
+          required: true
+          schema:
+            type: object
+            properties:
+              alert_group:
+                type: object
+                description: alert_group object
+                properties:
+                  email_group_ids:
+                    type: array
+                    items:
+                      type: integer
+                      format: int32
+                      description: update alert_group's email_group
+        responses:
+        "200":
+          description: successful operation
+        """
         ctxt = self.get_context()
         data = json_decode(self.request.body)
         alert_group_data = data.get('alert_group')
@@ -65,6 +240,33 @@ class AlertGroupHandler(ClusterAPIHandler):
 
     @gen.coroutine
     def delete(self, alert_group_id):
+        """
+        ---
+        tags:
+        - alert_group
+        summary: Delete the alert_group by id
+        description: delete alert_group by id
+        operationId: alertgroups.api.deleteAlertGroup
+        produces:
+        - application/json
+        parameters:
+        - in: header
+          name: X-Cluster-Id
+          description: Cluster ID
+          schema:
+            type: string
+          required: true
+        - in: url
+          name: id
+          description: Alert_group's id
+          schema:
+            type: integer
+            format: int32
+          required: true
+        responses:
+        "200":
+          description: successful operation
+        """
         ctxt = self.get_context()
         client = self.get_admin_client(ctxt)
         alert_group = yield client.alert_group_delete(ctxt, alert_group_id)
