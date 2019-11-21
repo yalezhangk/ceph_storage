d1acb8ca2b715d1c8e61e7596ab37c69d0223304
wanghongxu <wanghongxu@t2cloud.net>
2019-11-21 12:25:55 +0800

    fe(task): add probe

---
+++ DSpace/taskflows/probe.py
@@ -0,0 +1,35 @@
+import logging
+from pathlib import Path
+
+import paramiko
+
+from DSpace.tools.base import SSHExecutor
+from DSpace.tools.probe import ProbeTool
+
+logger = logging.getLogger(__name__)
+
+
+class ProbeTask(object):
+    ctxt = None
+    node = None
+    host_perfix = None
+
+    def __init__(self, ctxt, node, host_prefix=None):
+        self.host_prefix = host_prefix
+        self.ctxt = ctxt
+        self.node = node
+
+    def get_ssh_executor(self):
+        return SSHExecutor(hostname=str(self.node.ip_address),
+                           password=self.node.password)
+
+    def get_ssh_key(self):
+        home = str(Path.home())
+        pk = paramiko.RSAKey.from_private_key(open('%s/.ssh/id_rsa' % home))
+        return pk
+
+    def probe_cluster_nodes(self):
+        ssh = self.get_ssh_executor()
+        probe_tool = ProbeTool(ssh)
+        result = probe_tool.probe_cluster_nodes()
+        return result
