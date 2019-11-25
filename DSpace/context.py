c9d5f04336125f928a7a1d5b85aa057fff344f3d
wanghongxu <wanghongxu@t2cloud.net>
2019-11-26 01:17:09 +0800

    fe(request_id): add request_id for api

--- DSpace/context.py
+++ DSpace/context.py
@@ -1,34 +1,40 @@
+import uuid
+
+from oslo_context import context
+
 from DSpace.i18n import _
 
 
-class RequestContext(object):
-    def __init__(self, user_id=None, project_id=None, is_admin=False,
-                 read_deleted='no', cluster_id=None, client_ip=None):
-        self.user_id = user_id
-        self.project_id = project_id
-        self.is_admin = is_admin
+class RequestContext(context.RequestContext):
+    def __init__(self, read_deleted='no', cluster_id=None, client_ip=None,
+                 **kwargs):
+        request_id = kwargs.pop("request_id", None)
+        if not request_id:
+            request_id = str(uuid.uuid4())
         self.read_deleted = read_deleted
         self.cluster_id = cluster_id
         self.client_ip = client_ip
+        super(RequestContext, self).__init__(
+            request_id=request_id, **kwargs)
 
     def to_dict(self):
-        return {
+        res = super(RequestContext, self).to_dict()
+        res.update({
             "user_id": self.user_id,
-            "project_id": self.project_id,
             "read_deleted": self.read_deleted,
-            "is_admin": self.is_admin,
             "cluster_id": self.cluster_id,
             "client_ip": self.client_ip,
-        }
+        })
+        return res
 
     @classmethod
     def from_dict(cls, values):
         return cls(user_id=values['user_id'],
-                   project_id=values['project_id'],
                    read_deleted=values['read_deleted'],
                    cluster_id=values['cluster_id'],
                    client_ip=values.get('client_ip'),
-                   is_admin=values['is_admin'])
+                   is_admin=values['is_admin'],
+                   request_id=values['request_id'])
 
     def _get_read_deleted(self):
         return self._read_deleted
