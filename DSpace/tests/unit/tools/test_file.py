6a51270de66d8e2e73c7b668139d452e31da02f7
wanghongxu <wanghongxu@t2cloud.net>
2019-11-27 04:53:23 +0800

    fix(osd): config read error

--- DSpace/tests/unit/tools/test_file.py
+++ DSpace/tests/unit/tools/test_file.py
@@ -14,9 +14,9 @@ class TestFileTool(test.TestCase):
     @mock.patch.object(Executor, 'write')
     def test_file_write(self, run_command):
         run_command.return_value = (True)
-        tool = File(Executor())
+        tool = File(Executor(host_prefix="/a"))
         tool.write("a", "content")
-        run_command.assert_called_once_with("a", "content")
+        run_command.assert_called_once_with("/a/a", "content")
 
     @mock.patch.object(Executor, 'run_command')
     def test_mkdir(self, run_command):
