ad02ea5deb884f9407ce59c445ca8c9f8bb9df9e
zhangke <zhangke@t2cloud.net>
2019-11-27 15:38:23 +0800

    add(sysconfig): add action log

--- DSpace/DSM/sys_config.py
+++ DSpace/DSM/sys_config.py
@@ -3,6 +3,8 @@ from oslo_log import log as logging
 from DSpace import exception
 from DSpace import objects
 from DSpace.DSM.base import AdminBaseHandler
+from DSpace.objects.fields import AllActionType as Action
+from DSpace.objects.fields import AllResourceType as Resource
 from DSpace.taskflows.node import NodeTask
 
 logger = logging.getLogger(__name__)
@@ -23,7 +25,7 @@ class SysConfigHandler(AdminBaseHandler):
                 result[sysconf.key] = sysconf.value
         return result
 
-    def _update_chrony(self, ctxt, chrony_server):
+    def _update_chrony(self, ctxt, chrony_server, begin_action=None):
         nodes = objects.NodeList.get_all(ctxt)
         if not nodes:
             logger.error("no nodes to update chrony server")
@@ -32,8 +34,14 @@ class SysConfigHandler(AdminBaseHandler):
                 task = NodeTask(ctxt, node)
                 # TODO Use ordered taskflow to execute update chrony
                 self.task_submit(task.chrony_update())
+                status = 'success'
+                self.finish_action(begin_action, None, 'sysconfig',
+                                   chrony_server, status)
             except exception.StorException as e:
                 logger.error("update chrony server error: %s", e)
+                status = 'fail'
+                self.finish_action(begin_action, None, 'sysconfig',
+                                   chrony_server, status, err_msg=str(e))
                 raise e
 
     def update_sysinfo(self, ctxt, sysinfos):
@@ -49,9 +57,12 @@ class SysConfigHandler(AdminBaseHandler):
             if old_chrony != chrony_server:
                 logger.info("trying to update chrony_server from %s to %s",
                             old_chrony, chrony_server)
+                begin_action = self.begin_action(ctxt, Resource.SYSCONFIG,
+                                                 Action.UPDATE_CLOCK_SERVER)
                 objects.sysconfig.sys_config_set(ctxt, 'chrony_server',
                                                  chrony_server)
-                self.task_submit(self._update_chrony, ctxt, chrony_server)
+                self.task_submit(self._update_chrony, ctxt, chrony_server,
+                                 begin_action)
         if cluster_name:
             objects.sysconfig.sys_config_set(ctxt, 'cluster_name',
                                              cluster_name)
@@ -65,8 +76,11 @@ class SysConfigHandler(AdminBaseHandler):
             objects.sysconfig.sys_config_set(ctxt, 'cluster_cidr',
                                              cluster_cidr)
         if gateway_cidr:
+            begin_action = self.begin_action(ctxt, Resource.SYSCONFIG,
+                                             Action.UPDATE_CLOCK_SERVER)
             objects.sysconfig.sys_config_set(ctxt, 'gateway_cidr',
                                              cluster_cidr)
+            self.finish_action(begin_action, None, 'sysconfig', gateway_cidr)
 
     def image_namespace_get(self, ctxt):
         image_namespace = objects.sysconfig.sys_config_get(ctxt,
