73ada9adff4e88593b88ac369824063e05976543
zhangke <zhangke@t2cloud.net>
2019-11-29 14:57:13 +0800

    fix(test_mail): send test mail

--- DSpace/utils/mail.py
+++ DSpace/utils/mail.py
@@ -16,15 +16,15 @@ def send_mail(subject, content, config):
     smtp_to_email = config.get("smtp_to_email")
     smtp_name = config.get("smtp_name")
     smtp_subject = subject.get("smtp_subject")
-    smtp_context = content.get("smtp_context")
+    smtp_context = content.get("smtp_content")
     try:
         msg = MIMEText(smtp_context, 'plain', 'utf-8')
         msg['From'] = formataddr(
             [smtp_name,
              smtp_user])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
         msg['To'] = formataddr(
-            [smtp_to_email])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-        msg[smtp_subject] = smtp_subject  # 邮件的主题，也可以说是标题
+            [smtp_to_email, smtp_to_email])  # 对应收件人邮箱昵称、收件人邮箱账号
+        msg['subject'] = smtp_subject  # 邮件的主题，也可以说是标题
         if smtp_enable_ssl is True:
             server = smtplib.SMTP_SSL(smtp_host, smtp_port)
             server.login(smtp_user,
@@ -43,3 +43,4 @@ def send_mail(subject, content, config):
         server.quit()  # 关闭连接
     except Exception as e:
         logger.error("send mail error: %s", e)
+        raise e
