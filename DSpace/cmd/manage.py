d253810ba25b85863adc5eba9f05b68009512cf5
zhangke <zhangke@t2cloud.net>
2020-04-15 10:51:36 +0800

    add(permission): add block_page

--- DSpace/cmd/manage.py
+++ DSpace/cmd/manage.py
@@ -189,6 +189,7 @@ class DbCommands(object):
             ConfigKey.DISABLE_LICENSE: s_fields.ConfigType.BOOL,
             ConfigKey.ENABLE_OBJS_PAGE: s_fields.ConfigType.BOOL,
             ConfigKey.POOL_ID_SAME_AS_NAME: s_fields.ConfigType.BOOL,
+            ConfigKey.ENABLE_BLOCKS_PAGE: s_fields.ConfigType.BOOL,
         }
         for c in configs:
             key, value = c.split("=", 1)
